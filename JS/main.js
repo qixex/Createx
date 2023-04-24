b4bb5ed4844b5791cd88854aef69854ce5d80bbf
qixex <125477235+qixex@users.noreply.github.com>
2023-04-24 22:40:18 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
 // Type Iframe - видео Youtube (или Vimeo), карты Гугл или другой контент в iframe 
-$('.popup-youtube').magnificPopup({ 
-    type: 'iframe' 
+$('.popup-youtube').magnificPopup({
+    type: 'iframe'
 });
 
-$(function() {
+$(function () {
     var mixer = mixitup('.directions__list');
 
     $('.directions__filter-btn').on('click', function () {
@@ -18,7 +18,7 @@ $(function() {
         infinite: true,
         draggable: false,
         waitForAnimate: false,
-        
+
     })
     $('.team__slider-prev').on('click', function (e) {
         e.preventDefault()
@@ -28,7 +28,7 @@ $(function() {
         e.preventDefault()
         $('.team__slider').slick('slickNext')
     })
-    
+
 
     $('.testimonials__slider').slick({
         arrows: false,
@@ -36,13 +36,29 @@ $(function() {
         dots: true,
         appendDots: $('.testimonials__dots')
     })
-    $('.testimonial__prev').on('click', function (e) {
+    $('.testimonials__prev').on('click', function (e) {
         e.preventDefault()
         $('.testimonials__slider').slick('slickPrev')
     })
-    $('.testimonial__next').on('click', function (e) {
+    $('.testimonials__next').on('click', function (e) {
         e.preventDefault()
         $('.testimonials__slider').slick('slickNext')
+    })
+
+
+    $('.program__acc-link').on('click', function (e) {
+        e.preventDefault()
+        if ($(this).hasClass('program__acc-link--active')) {
+            $(this).removeClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideUp()
+        }
+        else {
+            $('.program__acc-link').removeClass('program__acc-link--active')
+            $('.program__acc-text').slideUp()
+            $(this).addClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideDown()
+            
+        }
     })
 
 })
